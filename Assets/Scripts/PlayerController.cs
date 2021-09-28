43612af3105814f22e2370a94198a493911f11e7
YasinFirat <yasin_firat0233@hotmail.com>
2021-09-29 02:15:18 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Wheels frontWheels; //Wheels sınıfı frontWheels değişken olarak eklendik
+    public float speed = 5f;
+    public float turnSpeed;
+    public float horizontalInput;
+    private float forwardInput;
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        if (forwardInput != 0) //Eğer forwardInput 0'dan farklı ise araç dönsün. 
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speed * horizontalInput);
+        }
+
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
+
+      
+        
+        //Her iki tekerlek döndürülür.
+        frontWheels.SetAngle(horizontalInput).TurnRight().TurnLeft(); 
+
+    }
+}
---
+++ Assets/Scripts/Wheels.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[System.Serializable]   //Sınıfı inspector panelinde değişken olarak gösterilmesini sağlar
+public struct Wheels
+{ //struct sınıfı, class sınıfına göre daha optimize olduğundan tercih ettim.
+    [Tooltip("Sağ tekeri sürükle")]
+    public Transform right;
+    [Tooltip("Sol tekeri sürükle")]
+    public Transform left;
+    [Tooltip("Tekerin Max dönme açısı")]
+    public float maxAngle;
+    private float angle;
+    
+    public Wheels SetAngle(float angle)
+    {
+        this.angle = angle;
+        return this; //Wheels sınıfını tekrar çağırır.
+    }
+    public Wheels TurnRight()
+    {
+        right.localRotation = Quaternion.Euler(0,maxAngle* angle, 0);
+        return this;//Wheels sınıfını tekrar çağırır.
+    } 
+    public Wheels TurnLeft()
+    {
+        left.localRotation = Quaternion.Euler(0,maxAngle* angle, 0);
+        return this;//Wheels sınıfını tekrar çağırır.
+    }
+
+}
